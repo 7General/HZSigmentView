34b8304d14c3ea81a26ec7c415e168d76eaef0ea
王会洲 <wanghuizhou21@163.com>
2016-05-26 15:51:22 +0800

    基本文件

---
+++ HZSigmentView/HZSigmentView/MainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainViewController.m
+//  HZSigmentView
+//
+//  Created by 王会洲 on 16/5/26.
+//  Copyright © 2016年 王会洲. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"HZSigmentView";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
